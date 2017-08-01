ffedca2a5b0ce473871dc9e86b0c2790ae2634ed
Ivan Gorbunov <meff34@gmail.com>
2017-08-01 21:05:13 +0300

    handleApiError for searchStation

--- source/doubleGisModules/searchStation.js
+++ source/doubleGisModules/searchStation.js
@@ -5,8 +5,13 @@ function searchStation(queryString) {
   const augmentedQueryString = `СПБ ${queryString} метро`;
   const queryUrl = config.doubleGisSearchUrl(augmentedQueryString);
 
-  // TODO: handleApiError
   return request.get(queryUrl)
+    .then((data) => {
+      if (data.response_code !== '200') {
+        return Promise.reject(new Error(`API error: ${data.error_message}`));
+      }
+      return Promise.resolve(data);
+    })
     .then((data, err) => new Promise((resolve, reject) => {
       if (err) reject(err);
       resolve(data.result[0].id);
