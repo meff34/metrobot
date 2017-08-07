dd5628e66e6bfefada4b53959dff21564cadfd26
Jhonny <meff34@gmail.com>
2017-08-07 23:10:22 +0300

    remove build from repo

--- build/utils/responseFormatter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function responseFormatter(schedule) {
-    return `Станция метро '${schedule.stationName}' работает\nс ${schedule.start} до ${schedule.end} 🚇`;
-}
-exports.default = responseFormatter;
